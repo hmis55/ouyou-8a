c939fa54a09e627729910b286da482bf7eb1b2a9
hmis55 <158746092+hmis55@users.noreply.github.com>
2024-03-10 06:01:22 +0000

    7a　完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,8 +11,14 @@ class BooksController < ApplicationController
 
   def index
     @user = current_user
+    #過去一週間のいいねの数の合計
     @book = Book.new
-    @books = Book.all
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorited_users).
+      sort_by {|x|
+        x.favorited_users.includes(:favorites).where(created_at: from...to).size
+      }.reverse
   end
 
   def create
